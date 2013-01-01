b0fd76c7f5d49415c36522da46efa7e4bbb12c78
EasonQian <weiqian.academic.gmail.com>
2013-01-01 19:51:35 +0800

    first commit

---
+++ HelloGit/src/eason/seu/edu/GitHello.java
@@ -0,0 +1,10 @@
+package eason.seu.edu;
+
+public class GitHello {
+	
+	private static final String output = "This is my first git demo using ssh.";
+	
+	public static void main(String[] args){
+		System.out.println(output);
+	}
+}
